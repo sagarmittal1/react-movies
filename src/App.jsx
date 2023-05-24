5490bafc9051ca85d590e616db70abdebf89641c
Sagar Mittal <mittalsagar006@gmail.com>
2023-05-24 11:49:37 +0530

    set up react router & made starting files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useEffect } from 'react';
-import { fetchDataFromApi } from './utils/api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { fetchDataFromApi } from './utils/api';
 import { getApiConfiguration } from './store/homeSlice';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
 
 function App() {
   const dispatch = useDispatch();
@@ -18,7 +27,19 @@ function App() {
     });
   };
 
-  return <>App{url?.total_pages}</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
