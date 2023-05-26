274b30269bc8ab7a8c2c712863e220b069f64937
Sagar Mittal <mittalsagar006@gmail.com>
2023-05-27 00:52:54 +0530

    completed footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
+
+import ContentWrapper from '../contentWrapper/ContentWrapper';
+
 import './style.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Unleash the power of Movix, your all-in-one movie destination.
+          Trending movies, comprehensive search, and detailed film information –
+          all at your fingertips. Dive into a world of cinematic wonders and
+          elevate your movie-watching experience like never before. Discover &
+          Explore and let the magic begin with MovixNow!
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
